997f2893ba252d74979c6c676e88cdb81df86021
Qijun Jiang <hehefeel@gmail.com>
2022-03-28 08:12:13 -0400

    Initial Commit

---
+++ MovieQuotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieQuotes
+//
+//  Created by Qijun Jiang on 2022/3/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
